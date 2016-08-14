d46fb4829ebc7868e8e4fb5850165f99a9d22b80
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-08-13 23:29:26 -0300

    create the methods: createExercise(), createExerciseRepetition(), cursorToExercise() and cursorToExerciseRepetition() in MyDataSource

--- app/src/main/java/com/example/rodri/letsworkout/database/MyDataSource.java
+++ app/src/main/java/com/example/rodri/letsworkout/database/MyDataSource.java
@@ -1,8 +1,14 @@
 package com.example.rodri.letsworkout.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+
+import com.example.rodri.letsworkout.model.Exercise;
+import com.example.rodri.letsworkout.model.ExerciseRepetition;
+import com.example.rodri.letsworkout.model.MuscleGroup;
 
 /**
  * Created by rodri on 8/12/2016.
@@ -64,5 +70,79 @@ public class MyDataSource {
 
     /** ----------  CREATE  ---------- */
 
+    public Exercise createExercise(long muscleGroupId, String name) {
+        ContentValues values = new ContentValues();
+        values.put(MySQLiteHelper.COLUMN_MUSCLE_GROUP_ID, muscleGroupId);
+        values.put(MySQLiteHelper.KEY_NAME, name);
+
+        long insertId = database.insert(MySQLiteHelper.TABLE_EXERCISES, null, values);
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_EXERCISES, exercisesColumns,
+                MySQLiteHelper.KEY_ID + " = " + insertId, null, null, null, null, null);
+
+        if (isCursorEmpty(cursor)) {
+            System.out.println("ERROR! Cursor is empty!");
+            cursor.close();
+            return null;
+        }
+        cursor.moveToFirst();
+
+        Exercise newExercise = cursorToExercise(cursor);
+        cursor.close();
+
+        return newExercise;
+    }
+
+    public ExerciseRepetition createExerciseRepetition(long exerciseId, int sets, int reps) {
+        ContentValues values = new ContentValues();
+        values.put(MySQLiteHelper.COLUMN_EXERCISE_ID, exerciseId);
+        values.put(MySQLiteHelper.COLUMN_SETS, sets);
+        values.put(MySQLiteHelper.COLUMN_REPS, reps);
+
+        long insertId = database.insert(MySQLiteHelper.TABLE_EXERCISE_REPETITIONS, null, values);
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_EXERCISE_REPETITIONS, exerciseRepetitionsColumns,
+                MySQLiteHelper.KEY_ID  + " = " + insertId, null, null, null, null, null);
+
+        if (isCursorEmpty(cursor)) {
+            System.out.println("ERROR! Cursor is empty!");
+            cursor.close();
+            return null;
+        }
+        cursor.moveToFirst();
+
+        ExerciseRepetition newExerciseRepetition = cursorToExerciseRepetition(cursor);
+        cursor.close();
+
+        return newExerciseRepetition;
+
+    }
+
+    /** ---------  CURSOR TO  ---------- */
+
+    public Exercise cursorToExercise(Cursor cursor) {
+        Exercise exercise = new Exercise();
+        exercise.setId(cursor.getLong(0));
+        exercise.setMuscleGroupId(cursor.getLong(1));
+        exercise.setName(cursor.getString(2));
+        return exercise;
+    }
+
+    public ExerciseRepetition cursorToExerciseRepetition(Cursor cursor) {
+        ExerciseRepetition exerciseRepetition = new ExerciseRepetition();
+        exerciseRepetition.setId(cursor.getLong(0));
+        exerciseRepetition.setExerciseId(cursor.getLong(1));
+        exerciseRepetition.setSets(cursor.getInt(2));
+        exerciseRepetition.setReps(cursor.getInt(3));
+        return exerciseRepetition;
+    }
+
+    /** ----------  OTHER  ---------- */
+
+    public boolean isCursorEmpty(Cursor cursor) {
+        if (cursor.moveToFirst()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
 }
