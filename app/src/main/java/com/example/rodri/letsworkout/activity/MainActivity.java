142631b57794ec5c5ce813a932ffc9b05e6c171f
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-08-16 15:59:39 -0300

    create drawerToggle and its events (onDrawerOpened and onDrawerClosed)

--- app/src/main/java/com/example/rodri/letsworkout/activity/MainActivity.java
+++ app/src/main/java/com/example/rodri/letsworkout/activity/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.rodri.letsworkout.activity;
 
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 
 import com.example.rodri.letsworkout.R;
@@ -18,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private ListView drawerListView;
-    private ActionBarDrawerToggle drawerToggle; // need to look after it
+    private ActionBarDrawerToggle drawerToggle; // need to look for it later
 
     private CharSequence drawerTitle;
     private CharSequence title;
@@ -37,6 +43,43 @@ public class MainActivity extends AppCompatActivity {
         // Initialize Views and other variables
         initialize();
         createDrawerItems();
+
+        adapter = new DrawerItemAdapter(this, 0, drawerItems);
+        drawerListView.setAdapter(adapter);
+
+        // Enable Action Bar and make it behave as a Toggle Button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // "Gambiarra" used to change the Navigation Drawer Icon
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_drawer, getTheme());
+        getSupportActionBar().setHomeAsUpIndicator(drawable);
+
+        Toolbar toolbar = new Toolbar(this);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name) {
+
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(title);
+                // call onPreparedOptionsMenu() to show action bar icons
+                // need to look for it later
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(drawerTitle);
+                // call onPreparedOptionsMenu() to hide action bar icons
+                // need to look for it later
+                invalidateOptionsMenu();
+            }
+
+        };
+
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        if (savedInstanceState == null) {
+            displayView(0);
+        }
     }
 
     public void initialize() {
@@ -60,5 +103,14 @@ public class MainActivity extends AppCompatActivity {
         drawerItems.add(new DrawerItem(menuTitles[4], menuIcons.getResourceId(4, -1)));
 
         menuIcons.recycle();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return true;
     }
 }
