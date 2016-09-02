46b24c77e687ccc08a84e1f7bb5cc7ddccabbcc5
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-09-02 12:55:52 -0300

    create MuscleGroupAdapter. Change layout from dialog_muscle_group.xml

--- app/src/main/java/com/example/rodri/letsworkout/activity/NewRoutineActivity.java
+++ app/src/main/java/com/example/rodri/letsworkout/activity/NewRoutineActivity.java
@@ -1,9 +1,13 @@
 package com.example.rodri.letsworkout.activity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,6 +18,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.rodri.letsworkout.R;
+import com.example.rodri.letsworkout.adapter.MuscleGroupAdapter;
 import com.example.rodri.letsworkout.database.MyDataSource;
 import com.example.rodri.letsworkout.util.Util;
 
@@ -40,8 +45,8 @@ public class NewRoutineActivity extends AppCompatActivity {
     private List<String> muscleGroup = new ArrayList<>();
     private List<String> exercises = new ArrayList<>();
     private ArrayAdapter<String> spinnerAdapterDays;
-    //private ArrayAdapter<String> spinnerAdapterMuscleGroup;
     private ArrayAdapter<String> spinnerAdapterExercises;
+    private MuscleGroupAdapter adapterMuscleGroup;
 
     private int selectedDay = -1;
     private int selectedMuscleGroup = -1;
@@ -78,7 +83,39 @@ public class NewRoutineActivity extends AppCompatActivity {
         etMuscleGroup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "It worked!", Toast.LENGTH_SHORT).show();
+
+                final Dialog dialog = new Dialog(NewRoutineActivity.this);
+                dialog.setContentView(R.layout.dialog_muscle_group);
+                dialog.setTitle("Muscle Group");
+
+                ListView listOfMuscleGroup = (ListView) dialog.findViewById(R.id.dialogMuscleGroup_listOfMuscleGroups);
+                Button btConfirm = (Button) dialog.findViewById(R.id.dialogMuscleGroup_btConfirm);
+
+                adapterMuscleGroup = new MuscleGroupAdapter(NewRoutineActivity.this, 0, muscleGroup);
+                listOfMuscleGroup.setAdapter(adapterMuscleGroup);
+
+                btConfirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        List<String> selectedItems = new ArrayList<>();
+                        selectedItems = adapterMuscleGroup.getSelectedItems();
+                        StringBuffer sb = new StringBuffer();
+                        for (int i = 0; i < selectedItems.size(); i++) {
+                            sb.append(selectedItems.get(i));
+                            if (i < selectedItems.size() - 1) {
+                                sb.append(" and ");
+                            }
+
+                        }
+                        etMuscleGroup.setHint(sb);
+                        Toast.makeText(NewRoutineActivity.this, "res: " + sb, Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+                    }
+                });
+
+                dialog.show();
+
+
             }
         });
 
---
+++ app/src/main/java/com/example/rodri/letsworkout/adapter/MuscleGroupAdapter.java
@@ -0,0 +1,93 @@
+package com.example.rodri.letsworkout.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.rodri.letsworkout.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rodri on 9/1/2016.
+ */
+public class MuscleGroupAdapter extends ArrayAdapter<String> {
+
+    private Activity activity;
+    private List<String> muscleGroup;
+    private LayoutInflater inflater = null;
+    private boolean[] selected;
+
+    public MuscleGroupAdapter(Activity activity, int textViewResourceId, List<String> muscleGroup) {
+        super(activity, textViewResourceId, muscleGroup);
+        try {
+            this.activity = activity;
+            this.muscleGroup = muscleGroup;
+            this.selected = new boolean[muscleGroup.size()];
+
+            inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return muscleGroup.size();
+    }
+
+    public class ViewHolder {
+        public TextView displayMuscleGroup;
+        public CheckBox displayCheck;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        ViewHolder holder = new ViewHolder();
+        if (convertView == null) {
+            v = inflater.inflate(R.layout.custom_muscle_group_item, null);
+
+            holder.displayMuscleGroup = (TextView) v.findViewById(R.id.customMuscleGroupItem_txtMuscleGroup);
+            holder.displayCheck = (CheckBox) v.findViewById(R.id.customMuscleGroupItem_check);
+
+            v.setTag(holder);
+        } else {
+            holder = (ViewHolder) v.getTag();
+        }
+
+        holder.displayMuscleGroup.setText(muscleGroup.get(position));
+        holder.displayCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selected[position] == true) {
+                    selected[position] = false;
+                } else {
+                    selected[position] = true;
+                }
+            }
+        });
+
+        return v;
+
+    }
+
+    public List<String> getSelectedItems() {
+        List<String> selectedItems = new ArrayList<>();
+        for (int i = 0; i < selected.length; i++) {
+            if (selected[i]) {
+                selectedItems.add(muscleGroup.get(i));
+            }
+        }
+
+        return selectedItems;
+    }
+}
